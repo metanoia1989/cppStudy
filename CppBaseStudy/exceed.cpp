acbf6a24cef255c4a6950fa3c319b425b0fdab68
Smith Adam <sogaxili@gmail.com>
2019-06-09 21:37:49 +0800

    干掉 exe文件

---
+++ CppBaseStudy/exceed.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#define ZERO 0
+#include <climits>
+
+int main()
+{
+    using namespace std;
+    short sam = SHRT_MAX;
+    unsigned short sue = sam;
+
+    cout << "Sam has " << sam << " dollars and Sue has " << sue;
+    cout << " dollars deposited." << endl
+         << "Add $1 to each account." << endl << "Now ";
+    sam = sam + 1;
+    sue = sue + 1;
+    
+    cout << "Sam has " << sam << " dollars and Sue has " << sue;
+    cout << " dollars deposited.\nPoor Sam!" << endl;
+}
