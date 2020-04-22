509ab94f2d1541678cd109711dbcb13145f39434
metanoia1989 <sogaxili@gmail.com>
2020-04-22 21:13:33 +0800

    添加其他图片查看器软件

--- CppBaseStudy/constants/IMO/constants.h
+++ CppBaseStudy/constants/IMO/constants.h
@@ -1,6 +1,17 @@
 #pragma once
 
+#include <iostream>
+
 namespace constants
 {
     const int PICTURE_NUMS = 5; 
+    class Number
+    {
+    public:
+        Number()
+        {
+            std::cout << "Number constructed" << std::endl; 
+        }
+    };
+    const Number price;
 } // namespace constants
--- CppBaseStudy/constants/IMO/main.cpp
+++ CppBaseStudy/constants/IMO/main.cpp
@@ -1,9 +1,12 @@
 #include "util1.hpp"
 #include "util2.hpp"
+// #include "constants.h"
 
 int main(int argc, char const *argv[])
 {
     util1::print();
     util2::print();
+    // std::cout << constants::PICTURE_NUMS << std::endl;
+    // Number num;
     return 0;
 }
