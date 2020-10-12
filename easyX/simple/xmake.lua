0b1ad00525169dcbe6dd928df40be4f641fc35b0
AdamSmith <sogaxili@gmail.com>
2020-10-12 09:24:26 +0800

    添加xmake option配置

--- easyX/simple/xmake.lua
+++ easyX/simple/xmake.lua
@@ -1,21 +1,50 @@
 add_rules("mode.debug", "mode.release")
 
-add_includedirs(
-    "D:/Program/VisualStudioCommunity2019/IDE/VC/Auxiliary/VS/include",
-    "D:/Program/VisualStudioCommunity2019/IDE/VC/Tools/MSVC/14.27.29110/include",
-    "C:/Program Files (x86)/Windows Kits/10/Include/10.0.18362.0/shared",
-    "C:/Program Files (x86)/Windows Kits/10/Include/10.0.18362.0/ucrt",
-    "C:/Program Files (x86)/Windows Kits/10/Include/10.0.18362.0/um"
-)
-add_linkdirs(
-    "D:/Program/VisualStudioCommunity2019/IDE/VC/Auxiliary/VS/lib/x64", 
-    "C:/Program Files (x86)/Windows Kits/10/Lib/10.0.18362.0/um/x64",
-    "C:/Program Files (x86)/Windows Kits/10/Lib/10.0.18362.0/ucrt/x64"
-)
+option("company")
+    set_default(false)
+    set_showmenu(true)
+    set_description("公司的easyX配置")
+option_end()
+
+option("home")
+    set_default(true)
+    set_showmenu(true)
+    set_description("家里的easyX配置")
+option_end()
+
+if is_config("home") then
+    add_includedirs(
+        "D:/Program/VisualStudioCommunity2019/IDE/VC/Auxiliary/VS/include",
+        "D:/Program/VisualStudioCommunity2019/IDE/VC/Tools/MSVC/14.27.29110/include",
+        "C:/Program Files (x86)/Windows Kits/10/Include/10.0.18362.0/shared",
+        "C:/Program Files (x86)/Windows Kits/10/Include/10.0.18362.0/ucrt",
+        "C:/Program Files (x86)/Windows Kits/10/Include/10.0.18362.0/um"
+    )
+    add_linkdirs(
+        "D:/Program/VisualStudioCommunity2019/IDE/VC/Auxiliary/VS/lib/x64", 
+        "C:/Program Files (x86)/Windows Kits/10/Lib/10.0.18362.0/um/x64",
+        "C:/Program Files (x86)/Windows Kits/10/Lib/10.0.18362.0/ucrt/x64"
+    )
+elseif is_config("company") then
+    add_includedirs(
+        "D:/Program/VistualStudio/IDE/VC/Auxiliary/VS/include",
+        "D:/Program/VistualStudio/IDE/VC/Tools/MSVC/14.27.29110/include",
+        "D:/Windows Kits/10/Include/10.0.18362.0/shared",
+        "D:/Windows Kits/10/Include/10.0.18362.0/ucrt",
+        "D:/Windows Kits/10/Include/10.0.18362.0/um",
+    )
+    add_linkdirs(
+        "D:/Program/VistualStudio/IDE/VC/Auxiliary/VS/lib/x64", 
+        "D:/Windows Kits/10/Lib/10.0.18362.0/um/x64",
+        "D:/Windows Kits/10/Lib/10.0.18362.0/ucrt/x64",
+    )
+end
+
 -- add_defines("UNICODE")
 -- add_links("EasyXw")
 add_links("EasyXa")
 add_links("Gdi32", "User32", "shell32", "Ole32")
+
 
 target("sample")
     set_kind("binary")
